714b90cb042a301c1303098eab9f5a4f3d989969
artem6742 <144213709+artem6742@users.noreply.github.com>
2023-10-04 12:05:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+# a = int(input())
+# print(a)
+# print(a + 1)
+# print(a + 2)
+
+#2
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a + b + c)
+
+#3
+# a = int(input())
+# v = a ** 3
+# s = 6 * a**2
+# print('Обьём = ', v)
+# print('площадь полной поверхности =', s)
+
+#4
+# a = int(input())
+# b = int(input())
+# f = 3 *((a +b) **3 )+ 275 * (b ** 2) - 127 * a - 41
+# print(f)
+
+#5
+# a = int(input())
+# print('cледующее за числом',a,'число: ',a + 1,)
+# print('для числа', a ,'предыдущее число: ', a - 1)
+
+#6
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print((a + b + c + d) * 3)
+
+#7
+# a_1 = int(input('первый член прогреси: '))
+# d = int(input('d'))
+# n = int(input('n'))
+# print(a_1 + d * (n - 1))
+
+#8
+# a = int(input())
+# print(a,'--',a * 2,'--',a * 3,'--',a * 4 ,'--', a * 5)
+
+#9
+# n = int(input('количество школников: '))
+# k = int(input('количество мандаринов: '))
+# print('количество целых мандаринов: ', k // n)
+# print('остаток: ', k % n)
+
+#10
+#a = int(input())
+#print((a + 1) // 2)
+
+#11
+# a = int(input('минуты: '))
+# b = a // 60
+# x = a - b * 60
+# print(a,'мин - это', b,'час',x,'минут')
+
+#12
+#n,s,p=int(input()),0,1
+#while n>0: c=n%10; p*=c; s+=c; n//=10
+#print('Сумма цифр =',s)
+#print('Произведение цифр =',p)
+
+#13
+#a ="""            _~_
+#           (0 0)
+#         /   v   \
+#        / (  _  ) \
+#           ^^ ^^     """
+#h = int(input("Number: "))
+#while(h > 0):
+#    print(a)
+#    h -= 1
+
+#14
+#a = int(input())
+#print(a // 10)
+
+#15
+#a = int(input('минуты: '))
+#b = a // 60
+#x = a - b * 60
+#print(b,":",x)
+
+#16
+#a = int(input())
+#if a == 0:
+#    print(1)
+#elif a == 1:
+#   print(0)
+#else:
+#    print('введено неправильное число')
